a4a3ccc9128810ebd97a64e7c2e72c897df63b44
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-04-11 16:46:10 +0200

    todo error

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { OlympicCountry } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<OlympicCountry[]>([]);
 
   constructor(private http: HttpClient) {}
 
@@ -16,11 +17,9 @@ export class OlympicService {
     return this.http.get<any>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
-        // TODO: improve error handling
-        console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+        console.error('Error to get data', error);
+        this.olympics$.next([]);
+        return of(null);
       })
     );
   }
